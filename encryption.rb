806ec14b34345088407bc9f436b1a04b0ad68ffb
mark1n0 <127632468+mgxy0@users.noreply.github.com>
2024-06-03 18:46:26 +0200

    Update encryption.rb

--- encryption.rb
+++ encryption.rb
@@ -1,19 +1,29 @@
 require 'openssl'
 require_relative 'utils'
 
-def encrypt(data, key, iv)
+def encrypt(data, password, iv)
+  salt = generate_iv
+  key = generate_key(password, salt)
+  password_hash = generate_password_hash(password, salt)
   cipher = OpenSSL::Cipher.new('AES-256-CBC')
   cipher.encrypt
   cipher.key = key
   cipher.iv = iv
   encrypted = cipher.update(data) + cipher.final
   hmac = OpenSSL::HMAC.digest('sha256', key, encrypted)
-  hmac + encrypted
+  salt + password_hash + hmac + encrypted
 end
 
-def decrypt(data, key, iv)
-  hmac = data[0, 32]
-  encrypted = data[32..-1]
+def decrypt(data, password, iv)
+  salt = data[0...16]
+  stored_password_hash = data[16...48]
+  hmac = data[48...80]
+  encrypted = data[80..-1]
+  key = generate_key(password, salt)
+  password_hash = generate_password_hash(password, salt)
+  
+  raise "Password validation failed" unless stored_password_hash == password_hash
+  
   valid_hmac = OpenSSL::HMAC.digest('sha256', key, encrypted)
   
   raise "HMAC validation failed" unless hmac == valid_hmac
