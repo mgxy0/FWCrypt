a4771be2c6481d0317eb008c7d7ad0d1e6a55dc9
mark1n0 <127632468+mgxy0@users.noreply.github.com>
2024-06-03 11:50:08 +0200

    Update utils.rb

--- utils.rb
+++ utils.rb
@@ -1,31 +1,46 @@
-def fibonacci(n)
-  return n if n < 2
-  a, b = 0, 1
-  (n-1).times { a, b = b, a + b }
-  b
-end
+require 'openssl'
+require 'securerandom'
+require 'base64'
 
-def woodall(n)
-  n * (2 ** n) - 1
-end
-
-def generate_key
-  key = []
-  (1..16).each do |i|
-    key << (fibonacci(i) & 0xFF)
-    key << (woodall(i) & 0xFF)
+module Utils
+  def self.fibonacci(n)
+    (0..n).each_with_object([0, 1]) { |_, fib| fib << fib[-2] + fib[-1] }
   end
-  key.pack("C*").ljust(32, "\0") # Ensure the key is 32 bytes
-end
 
-def generate_iv
-  OpenSSL::Random.random_bytes(16) # 16 bytes for AES-256-CBC IV
-end
+  def self.woodall(n)
+    (1..n).map { |k| k * (2**k) - 1 }
+  end
 
-def read_file(path)
-  File.binread(path)
-end
+  def self.generate_key(salt)
+    fib = fibonacci(16).join
+    woodall = woodall(16).join
+    data = fib + woodall + salt
+    OpenSSL::PKCS5.pbkdf2_hmac_sha1(data, salt, 100_000, 32)
+  end
 
-def write_file(path, data)
-  File.binwrite(path, data)
+  def self.encrypt(data, key)
+    cipher = OpenSSL::Cipher.new('aes-256-cbc')
+    cipher.encrypt
+    iv = cipher.random_iv
+    cipher.key = key
+    cipher.iv = iv
+    encrypted = cipher.update(data) + cipher.final
+    hmac = OpenSSL::HMAC.digest('sha256', key, encrypted)
+    Base64.encode64(iv + encrypted + hmac)
+  end
+
+  def self.decrypt(encrypted_data, key)
+    encrypted_data = Base64.decode64(encrypted_data)
+    iv = encrypted_data[0..15]
+    hmac = encrypted_data[-32..-1]
+    encrypted = encrypted_data[16...-32]
+
+    raise "HMAC verification failed" unless hmac == OpenSSL::HMAC.digest('sha256', key, encrypted)
+
+    cipher = OpenSSL::Cipher.new('aes-256-cbc')
+    cipher.decrypt
+    cipher.key = key
+    cipher.iv = iv
+    decrypted = cipher.update(encrypted) + cipher.final
+  end
 end
