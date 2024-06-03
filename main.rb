dce050a3a46f2bf3046dba0f5e0cc37897b8145c
mark1n0 <127632468+mgxy0@users.noreply.github.com>
2024-06-03 11:55:39 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,27 +1,65 @@
 require_relative 'encryption'
-require 'securerandom'
+require_relative 'utils'
+require 'base64'
 
-if ARGV.length != 2
-  puts "Usage: ruby main.rb [encrypt|decrypt] [file_path]"
-  exit
+def main
+  puts "Do you want to (e)ncrypt or (d)ecrypt a file?"
+  choice = gets.strip.downcase
+
+  case choice
+  when 'e'
+    key = generate_key
+    iv = generate_iv
+
+    puts "Enter the path of the file to encrypt:"
+    input_path = gets.strip
+
+    puts "Enter the directory to save the encrypted file:"
+    output_dir = gets.strip
+
+    data = read_file(input_path)
+    original_filename = File.basename(input_path)
+    metadata = "#{original_filename}|"
+
+    combined_data = metadata + Base64.strict_encode64(data)
+
+    encrypted_data = encrypt(combined_data, key, iv)
+
+    output_path_enc = File.join(output_dir, "#{original_filename}.fwc")
+    write_file(output_path_enc, Base64.strict_encode64(iv + encrypted_data))
+
+    puts "Encrypted file saved at #{output_path_enc}"
+
+  when 'd'
+    puts "Enter the path of the encrypted file (.fwc):"
+    input_path_enc = gets.strip
+
+    unless input_path_enc.end_with?('.fwc')
+      puts "Error: The file to decrypt must have a .fwc extension"
+      return
+    end
+
+    key = generate_key
+
+    encrypted_data_with_iv = Base64.strict_decode64(read_file(input_path_enc))
+    iv = encrypted_data_with_iv[0...16]
+    encrypted_data = encrypted_data_with_iv[16..-1]
+
+    decrypted_data = decrypt(encrypted_data, key, iv)
+
+    metadata_end_index = decrypted_data.index('|')
+    metadata = decrypted_data[0...metadata_end_index]
+    file_data = Base64.strict_decode64(decrypted_data[(metadata_end_index + 1)..-1])
+
+    output_path_dec = File.join(File.dirname(input_path_enc), metadata)
+
+    write_file(output_path_dec, file_data)
+
+    puts "Decrypted file saved at #{output_path_dec}"
+
+  else
+    puts "Invalid choice. Please enter 'e' to encrypt or 'd' to decrypt."
+  end
 end
 
-action = ARGV[0]
-file_path = ARGV[1]
-
-salt = SecureRandom.hex(16)
-key = Encryption.generate_key(salt)
-
-if action == 'encrypt'
-  data = File.read(file_path)
-  encrypted_data = Encryption.encrypt(data, key)
-  File.write(file_path + '.enc', encrypted_data)
-  puts "File encrypted and saved as #{file_path}.enc"
-elsif action == 'decrypt'
-  encrypted_data = File.read(file_path)
-  decrypted_data = Encryption.decrypt(encrypted_data, key)
-  File.write(file_path.gsub('.enc', ''), decrypted_data)
-  puts "File decrypted and saved as #{file_path.gsub('.enc', '')}"
-else
-  puts "Unknown action: #{action}"
-end
+main
