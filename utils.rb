40745788f2e2be3a36bcd71db8425399eee26c3c
mark1n0 <127632468+mgxy0@users.noreply.github.com>
2024-06-03 18:45:58 +0200

    Update utils.rb

--- utils.rb
+++ utils.rb
@@ -11,7 +11,7 @@ def woodall(n)
   n * (2 ** n) - 1
 end
 
-def generate_complex_key
+def generate_complex_key(password)
   key_material = []
   (1..32).each do |i|
     fib = fibonacci(i) & 0xFF
@@ -19,16 +19,22 @@ def generate_complex_key
     combined = (fib ^ wood) % 256
     key_material << combined
   end
-  key_material.pack("C*")
+  combined_key = key_material.pack("C*") + password
+  combined_key
 end
 
-def generate_key(salt)
-  complex_key = generate_complex_key
+def generate_key(password, salt)
+  complex_key = generate_complex_key(password)
   OpenSSL::PKCS5.pbkdf2_hmac(complex_key, salt, 20000, 32, 'sha256')
 end
 
 def generate_iv
   OpenSSL::Random.random_bytes(16) # 16 bytes for AES-256-CBC IV
+end
+
+def generate_password_hash(password, salt)
+  combined_key = generate_complex_key(password)
+  OpenSSL::PKCS5.pbkdf2_hmac(combined_key, salt, 20000, 32, 'sha256')
 end
 
 def read_file(path)
