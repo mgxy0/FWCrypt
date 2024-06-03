e15962690618eff003f7cf4bf2a2c0d43f0f27c2
mark1n0 <127632468+mgxy0@users.noreply.github.com>
2024-06-03 16:23:06 +0200

    Update encryption.rb

--- encryption.rb
+++ encryption.rb
@@ -7,14 +7,21 @@ def encrypt(data, key, iv)
   cipher.key = key
   cipher.iv = iv
   encrypted = cipher.update(data) + cipher.final
-  encrypted
+  hmac = OpenSSL::HMAC.digest('sha256', key, encrypted)
+  hmac + encrypted
 end
 
 def decrypt(data, key, iv)
+  hmac = data[0, 32]
+  encrypted = data[32..-1]
+  valid_hmac = OpenSSL::HMAC.digest('sha256', key, encrypted)
+  
+  raise "HMAC validation failed" unless hmac == valid_hmac
+  
   cipher = OpenSSL::Cipher.new('AES-256-CBC')
   cipher.decrypt
   cipher.key = key
   cipher.iv = iv
-  decrypted = cipher.update(data) + cipher.final
+  decrypted = cipher.update(encrypted) + cipher.final
   decrypted
 end
